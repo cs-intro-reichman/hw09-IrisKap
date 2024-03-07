e5425bf9f4ed34af7a8b04840321fe1f87ae1f0a
Iris Kaplun <iris.kaplun@post.runi.ac.il>
2024-03-07 15:32:35 +0200

    initial commit

--- LanguageModel.java
+++ LanguageModel.java
@@ -3,6 +3,8 @@ import java.util.Random;
 
 public class LanguageModel {
 
+    
+
     // The map of this model.
     // Maps windows to lists of charachter data objects.
     HashMap<String, List> CharDataMap;
@@ -12,6 +14,8 @@ public class LanguageModel {
     
     // The random number generator used by this model. 
 	private Random randomGenerator;
+
+   
 
     /** Constructs a language model with the given window length and a given
      *  seed value. Generating texts from this model multiple times with the 
@@ -33,18 +37,98 @@ public class LanguageModel {
 
     /** Builds a language model from the text in the given file (the corpus). */
 	public void train(String fileName) {
-		// Your code goes here
+        String window = "";
+        char c;
+        In in = new In(fileName);
+
+        for(int i=0; i<windowLength; i++)
+        {
+            c= in.readChar();
+            window+=c;
+        }
+
+        
+
+        while (!in.isEmpty()) 
+        { 
+            c  = in.readChar();
+            List probs = new List();
+            if(CharDataMap.containsKey(window))
+            {
+                probs = CharDataMap.get(window);
+            }
+            else 
+            {
+                CharDataMap.put(window, probs);
+            }
+
+            probs.update(c);
+            CharDataMap.put(window, probs);
+
+            window+=c;
+            window = window.substring(1);
+        }
+
+        for (List probs : this.CharDataMap.values())
+        {
+              calculateProbabilities(probs);
+        }
 	}
 
     // Computes and sets the probabilities (p and cp fields) of all the
 	// characters in the given list. */
-	public void calculateProbabilities(List probs) {				
-		// Your code goes here
+	public void calculateProbabilities(List probs) {	
+        int length = 0;
+        Node current = probs.getFirstNode();
+		for(int i=0; i< probs.getSize(); i++)
+        {
+           length+= current.cp.count;
+           current= current.next;
+        }
+        System.out.println(length);
+
+        double singleProbability = 1.0/length;
+        System.out.println(singleProbability);
+
+        current = probs.getFirstNode();
+        double cp =0;
+        Node previous = null;
+        for(int i=0; i<probs.getSize() ; i++)
+        {
+            current.cp.p = singleProbability* current.cp.count;
+            if(previous==null)
+            {
+                current.cp.cp = current.cp.p;
+            }
+            else
+            {
+                current.cp.cp = previous.cp.cp + current.cp.p;
+            }
+            previous = current;
+            current = current.next;
+           
+        }
+        System.out.println(probs);
+
 	}
 
     // Returns a random character from the given probabilities list.
 	public char getRandomChar(List probs) {
-		// Your code goes here
+		
+        Node current = probs.getFirstNode();
+        double random = randomGenerator.nextDouble();
+
+        Node previous = null;
+
+        while(current!=null)
+        {
+            if(random< current.cp.cp)
+                return current.cp.chr;
+            previous = current;
+            current = current.next;
+        }
+
+        return previous.cp.chr; //incase random>0.9999..
 	}
 
     /**
@@ -69,6 +153,24 @@ public class LanguageModel {
 	}
 
     public static void main(String[] args) {
-		// Your code goes here
+
+        List test = new List();
+		test.addFirst(' ');
+        test.addFirst('e');
+        test.addFirst('t');
+        test.addFirst('i');
+        test.addFirst('m');
+        test.addFirst('o');
+        test.addFirst('c');
+        test.update('m');
+        test.update('t');
+        test.update('e');
+
+        LanguageModel a = new LanguageModel(0);
+        a.calculateProbabilities(test);
+        
+        //System.out.println(test);
     }
+
+    
 }
